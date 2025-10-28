a22b34b011989f2b4155c35bdf5bdbb178700553
cristian <cristianalvarez1496@gmail.com>
2025-10-28 17:13:58 -0500

    adding models

---
+++ src/main/java/com/zosh/modal/Order.java
@@ -0,0 +1,44 @@
+package com.zosh.modal;
+
+import com.zosh.domain.OrderStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String orderId;
+
+    @ManyToOne
+    private User user;
+
+    private Long sellerId;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    @ManyToOne
+    private Address shippingAddress;
+
+    @Embedded
+    private PaymentDetails paymentDetails = new PaymentDetails();
+
+    private double totalMrpPrice;
+
+    private Integer totalSellingPrice;
+
+    private Integer discount;
+
+    private OrderStatus orderStatus;
+}
---
+++ src/main/java/com/zosh/modal/PaymentDetails.java
@@ -0,0 +1,4 @@
+package com.zosh.modal;
+
+public class PaymentDetails {
+}
